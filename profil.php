e3a5ca223c028a29b951f4bb7fa3beaeed82a91c
MYRNA\Myrna <myrna.el-awady@edu.ece.fr>
2018-05-01 16:17:54 +0200

    new

--- profil.php
+++ profil.php
@@ -6,12 +6,12 @@ Node in
 <link type="text/css" rel="stylesheet" href="profil.css" />
     </head>
 <body >
- <div id = "accueil" >Accueil</div>
+
  <div class = "header1" >  
   <div id = "accueil" class = "header1">Accueil</div>
     <div id = "Reseau" class = "header1">Reseau</div>
     <div id = "Notifs" class = "header1">Notifs</div>
-    <div id = "LOL" class = "header1">Emplois</div>
+    <div id = "Emplois" class = "header1">Emplois</div>
     <div id = "Messages" class = "header1">Messages</div>
     <div id = "Moi" class = "header1">Moi</div>
     
