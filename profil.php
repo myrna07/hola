2de25c8436911eed027409da65e87a46dee9b53d
MYRNA\Myrna <myrna.el-awady@edu.ece.fr>
2018-05-01 22:23:04 +0200

    g la tête sous l'o

--- profil.php
+++ profil.php
@@ -15,7 +15,15 @@ Node in
     <div id = "Messages" class = "header1">Messages</div>
     <div id = "Moi" class = "header1">Moi</div>
      <div id="submit1" class="header1"><input type="submit" id="button3" value="Deconnexion"/></div>
+    
    </div>
+    <div class = "header2"></div>
+    <div class = "droite"></div>
+    <div class = "couv"><img src ="loli.png" width ="850px"/></div>
+    
+      <div class = "profil"></div>
+     <div class = "profilpic"><img src ="profil2.png" height ="140px"/></div>
+   
 </body>
 
 </html>
