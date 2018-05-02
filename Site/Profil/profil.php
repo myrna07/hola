5561a3b48b49d212ff47c9dfd7b71ef015b02bfa
Hoche Mathilde <mathilde.hoche@edu.ece.fr>
2018-05-02 02:52:21 +0200

    Page d'accueil/Header

---
+++ Site/Profil/profil.php
@@ -0,0 +1,33 @@
+<html>
+<title>
+Node in 
+</title>
+<head>
+<link type="text/css" rel="stylesheet" href="profil.css" />
+    </head>
+<body >
+
+ <div class = "header1" > 
+     <ul>
+     <li><a href="#" id="Accueil" class="header1">Accueil</a></li>
+	  <li><a href="#" id="Reseau" class="header1">Reseau</a></li>
+	  <li><a href="#"id="Notifications" class="header1">Notifications</a></li>
+	  <li><a href="#"id="Emplois" class="header1">Emplois</a></li>
+      <li><a href="#"id="Messages" class="header1">Messages</a></li>
+      <li><a href="#"id="Moi" class="header1">Moi</a></li>  
+         
+    <a href="Site.html"><img src="deco.png" id="img1" /></a>
+
+   </ul>
+    
+   </div>
+    <div class = "header2"></div>
+    <div class = "droite"></div>
+    <div class = "couv"><img src ="loli.png" width ="850px"/></div>
+    
+      <div class = "profil"></div>
+     <div class = "profilpic"><img src ="profil.png" height ="140px"/></div>
+   
+</body>
+
+</html>
